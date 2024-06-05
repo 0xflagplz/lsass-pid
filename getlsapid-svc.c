eb99d696c72503420afd5eb54d8d68147f2a8010
Ed <59506742+0xflagplz@users.noreply.github.com>
2024-06-05 10:17:32 -0700

    comments

--- getlsapid-svc.c
+++ getlsapid-svc.c
@@ -1,16 +1,16 @@
 #include <windows.h>
-#include <winsvc.h>  // Make sure to include this before using WINBOOL
+#include <winsvc.h>  
 #include "beacon.h"
 #define WINBOOL int
 
+// gotta link these bad boys (api calls) for the BOF linker in the C2
 WINBASEAPI DWORD WINAPI KERNEL32$GetLastError(VOID);
 WINADVAPI SC_HANDLE WINAPI ADVAPI32$OpenSCManagerW(LPCWSTR lpMachineName, LPCWSTR lpDatabaseName, DWORD dwDesiredAccess);
 WINADVAPI WINBOOL WINAPI ADVAPI32$QueryServiceStatusEx(SC_HANDLE hService, SC_STATUS_TYPE InfoLevel, LPBYTE lpBuffer, DWORD cbBufSize, LPDWORD pcbBytesNeeded);
 WINADVAPI WINBOOL WINAPI ADVAPI32$CloseServiceHandle(SC_HANDLE hSCObject);
 WINADVAPI SC_HANDLE WINAPI ADVAPI32$OpenServiceW(SC_HANDLE hSCManager, LPCWSTR lpServiceName, DWORD dwDesiredAccess);
-//
-// Query samss for LSA process ID.
-//
+
+// Query samss for LSA process ID
 DWORD GetLsaPidFromService(void) {
     SC_HANDLE              ManagerHandle = NULL, ServiceHandle = NULL;
     SERVICE_STATUS_PROCESS ProcessInfo;
@@ -19,17 +19,14 @@ DWORD GetLsaPidFromService(void) {
     BOOL                   Result;
 
     do {
-        ManagerHandle = ADVAPI32$OpenSCManagerW(
-                            NULL,
-                            NULL,
-                            SC_MANAGER_CONNECT
-                        );
+        // open the Service Control Manager
+        ManagerHandle = ADVAPI32$OpenSCManagerW(NULL, NULL, SC_MANAGER_CONNECT);
 
         if (!ManagerHandle) {
             BeaconPrintf(CALLBACK_OUTPUT, "OpenSCManager() failed : %ld\n", KERNEL32$GetLastError());
             break;
         }
-
+        // open the samss service
         ServiceHandle = ADVAPI32$OpenServiceW(
                             ManagerHandle,
                             L"samss",
@@ -40,6 +37,17 @@ DWORD GetLsaPidFromService(void) {
             BeaconPrintf(CALLBACK_OUTPUT, "OpenService() failed : %ld\n", KERNEL32$GetLastError());
             break;
         }
+        // query the service status to get process information and save to memory location of ProcessInfo
+        // for reference:
+        //
+        // BOOL QueryServiceStatusEx(
+        //   [in]            SC_HANDLE      hService,
+        //   [in]            SC_STATUS_TYPE InfoLevel,
+        //   [out, optional] LPBYTE         lpBuffer,  [buffer location to store into (our var ProcessInfo)]
+        //   [in]            DWORD          cbBufSize,
+        //   [out]           LPDWORD        pcbBytesNeeded
+        // );
+
 
         Result = ADVAPI32$QueryServiceStatusEx(
                     ServiceHandle,
@@ -53,14 +61,17 @@ DWORD GetLsaPidFromService(void) {
             BeaconPrintf(CALLBACK_OUTPUT, "QueryServiceStatusEx() failed : %ld\n", KERNEL32$GetLastError());
             break;
         }
-
+        // since we directed the api QueryServiceStatusEx to use the location &ProcessInfo 
+        // we can querythe specific parameter of dwProcessId for the processID
         ProcessId = ProcessInfo.dwProcessId;
     } while(FALSE);
 
+
+    // Close the service handle
     if (ServiceHandle) {
         ADVAPI32$CloseServiceHandle(ServiceHandle);
     }
-
+    // Close the SC Manager handle
     if (ManagerHandle) {
         ADVAPI32$CloseServiceHandle(ManagerHandle);
     }
